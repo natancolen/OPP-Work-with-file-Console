26c52c125a88f6dfe727af28d3f4e7ee9be83e2e
Natã Batista Brandão <natancolen@gmail.com>
2020-03-08 12:31:40 -0300

    Criado a classe Product

---
+++ ConsoleApp1/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1.Entities
+{
+    class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int amount { get; set; }
+
+        public Product(string name, double price, int amount)
+        {
+            this.name = name;
+            this.price = price;
+            this.amount = amount;
+        }
+
+        public double TotalCost()
+        {
+            return (amount * price) * 0.01;
+        }
+    }
+}
