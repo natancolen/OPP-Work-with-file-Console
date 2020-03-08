06e72cb008cebdace363b1c7eff1b9b4d4a7c572
Natã Batista Brandão <natancolen@gmail.com>
2020-03-08 13:01:13 -0300

    Ler e gera arquivo txt e pasta. Imprimi na tela os dados criados no novo arquivo

--- ConsoleApp1/Entities/Product.cs
+++ ConsoleApp1/Entities/Product.cs
@@ -1,4 +1,4 @@
-﻿namespace ConsoleApp1.Entities
+﻿namespace Entities
 {
     class Product
     {
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using Entities;
 
 namespace ConsoleApp1
 {
@@ -10,6 +9,39 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string sourceFilePath = @"productList.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string folderPath = @"out";
+                string targetFilePath = folderPath + "/summary.txt";
+
+                Directory.CreateDirectory(folderPath);
+
+                using (StreamWriter sw = new StreamWriter(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] split = line.Split(',');
+
+                        string name = split[0];
+                        double price = double.Parse(split[1]);
+                        int amount = int.Parse(split[2]);
+
+                        Product p = new Product(name, price, amount);
+
+                        Console.WriteLine(p.name + ", " + p.TotalCost().ToString("F2", CultureInfo.InvariantCulture));
+                        sw.WriteLine(p.name + ", " + p.TotalCost().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e);
+            }
         }
     }
 }
